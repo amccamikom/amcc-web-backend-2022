19d8e77a1be9849e3a717be6ee64da539227c9b0
malikfajr <malilfajar17@gmail.com>
2023-04-07 14:32:15 +0700

    feat: update db

--- pelatihan_backend.sql
+++ pelatihan_backend.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 14, 2023 at 12:23 PM
+-- Generation Time: Apr 07, 2023 at 07:11 AM
 -- Server version: 8.0.30
--- PHP Version: 7.4.33
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -51,6 +51,16 @@ CREATE TABLE `questions` (
   `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`id`, `username`, `title`, `content`, `created_at`, `updated_at`) VALUES
+(7, 'andi', 'Pertanyaan 1', 'Ini Pertanyaan 1?', '2023-04-06 13:51:04', '2023-04-06 13:51:04'),
+(8, 'andi', 'Pertanyaan 2', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Id non maxime provident consectetur officiis, error cum quae omnis dolore nihil repellat temporibus ex suscipit at quis! Ipsam dolor sed minima.', '2023-04-06 13:51:36', '2023-04-06 13:51:36'),
+(9, 'andi', 'Pertanyaan 3', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Id non maxime provident consectetur officiis, error cum quae omnis dolore nihil repellat temporibus ex suscipit at quis! Ipsam dolor sed minima.', '2023-04-06 13:51:50', '2023-04-06 13:51:50'),
+(10, 'andi', 'Pertanyaan 4', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Id non maxime provident consectetur officiis, error cum quae omnis dolore nihil repellat temporibus ex suscipit at quis! Ipsam dolor sed minima.', '2023-04-06 13:53:36', '2023-04-06 13:53:36');
+
 -- --------------------------------------------------------
 
 --
@@ -85,13 +95,16 @@ INSERT INTO `users` (`username`, `name`, `email`, `password`, `created_at`, `upd
 -- Indexes for table `answers`
 --
 ALTER TABLE `answers`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `username` (`username`),
+  ADD KEY `question_id` (`question_id`);
 
 --
 -- Indexes for table `questions`
 --
 ALTER TABLE `questions`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `username` (`username`);
 
 --
 -- Indexes for table `users`
@@ -114,7 +127,24 @@ ALTER TABLE `answers`
 -- AUTO_INCREMENT for table `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `answers`
+--
+ALTER TABLE `answers`
+  ADD CONSTRAINT `answers_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `answers_ibfk_2` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `questions`
+--
+ALTER TABLE `questions`
+  ADD CONSTRAINT `questions_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
